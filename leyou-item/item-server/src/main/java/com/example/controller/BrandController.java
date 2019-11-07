8ae6b47036566921df60d7b1fff2625b20d5eccd
jing <974375135@qq.com>
2019-11-07 10:04:50 +0800

    add //00000

--- leyou-item/item-server/src/main/java/com/example/controller/BrandController.java
+++ leyou-item/item-server/src/main/java/com/example/controller/BrandController.java
@@ -31,7 +31,7 @@ public class BrandController {
             return ResponseEntity.notFound().build();
         }
         //ResponseEntity e = ResponseEntity.ok();
-        //ggggggg111111gggggggg
+        //ggggggg00000gggggggg
         //????
         return ResponseEntity.ok(pageResult);
     }
