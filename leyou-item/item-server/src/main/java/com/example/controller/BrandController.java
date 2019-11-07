3a56eb2b897059f924b45d5998012aa947d6154b
Jing <974375135@qq.com>
2019-11-07 08:41:08 +0800

    Update BrandController.java

--- leyou-item/item-server/src/main/java/com/example/controller/BrandController.java
+++ leyou-item/item-server/src/main/java/com/example/controller/BrandController.java
@@ -30,6 +30,7 @@ public class BrandController {
         if (pageResult == null) {
             return ResponseEntity.notFound().build();
         }
+        ResponseEntity e = ResponseEntity.ok();
         return ResponseEntity.ok(pageResult);
     }
 }
