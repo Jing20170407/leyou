0e88607b1411d3721868a8e53c1cc954d6b922a1
Jing <974375135@qq.com>
2019-11-07 11:26:26 +0800

    new line  //000000000000000

--- leyou-item/item-server/src/main/java/com/example/controller/BrandController.java
+++ leyou-item/item-server/src/main/java/com/example/controller/BrandController.java
@@ -32,6 +32,7 @@ public class BrandController {
         }
         //ResponseEntity e = ResponseEntity.ok();
         //ggggggg55555555556666666666666666gggggggg
+        //ggggggg000000000000000000000000000gggggggg
         //????
         return ResponseEntity.ok(pageResult);
     }
